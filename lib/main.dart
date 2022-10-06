8f084e30bb64a6881e36fc49c8912ee01118e2cc
itsmepks21 <pks891618@gmail.com>
2022-10-07 01:10:11 +0530

    code optimisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,73 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
-void main() => runApp(VqcodesMain());
 
-class VqcodesMain extends StatefulWidget {
+import './quiz.dart';
+import './result.dart';
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _VqcodesMainState();
+    // TODO: implement createState
+    return _MyAppState();
   }
 }
 
-class _VqcodesMainState extends State<VqcodesMain> {
-    static const questions = [
-      {
-      'questiontext':'Whats \'s your favourite color ?'
-      ,'answers':['red','gree','black','orange']
-      },
-      {
-      'questiontext':'Whats \'s your favourite Instructor ?'
-      ,'answers':['Sahil','Pankaj','Rajan','MAx'],
-      },
-      {
-      'questiontext':'Whats \'s your favourite animal ?'
-      ,'answers':['Dog','Cat','Cow','Tiger'],
-      }
-   
-    ];
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': ['Max', 'Max', 'Max', 'Max'],
+    },
+  ];
   var _questionIndex = 0;
+
   void _answerQuestion() {
-  
-    if(_questionIndex < questions.length){
-        print("We have more questions ");
-    }
+    // var aBool = true;
+    // aBool = false;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-  
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("My First App!!  VQCODES")),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['questiontext']),
-           ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-            return Answer(_answerQuestion,answer);
-           }).toList()
-          ],
+        appBar: AppBar(
+          title: Text('My First App'),
         ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
       ),
     );
   }
---
+++ lib/quiz.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+class Quiz extends StatelessWidget {
+
+  final List<Map<String,Object>> questions;
+  final questionIndex; 
+  final Function answerQuestion;  //final VoidCallback answerQuestion;
+
+  Quiz({@required this.answerQuestion,@required this.questionIndex,@required this.questions});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+          children: [
+            Question(questions[questionIndex]['questiontext']),
+           ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+            return Answer(answerQuestion,answer);
+           }).toList()
+          ],
+        );
+  }
+}
